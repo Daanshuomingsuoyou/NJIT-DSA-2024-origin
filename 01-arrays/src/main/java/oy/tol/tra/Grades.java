6e52fa798a6fabaa5f533f462e1f2fb366a94607
Daanshuomingsuoyou <1906718207@qq.com>
2024-03-26 10:50:25 +0800

    SUBMIT

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -23,52 +23,14 @@ public class Grades {
     * The method to reverse the internal Java int array.
     */
    public void reverse() {
-      /* TODO:
-       1. Edit the test data files to see if the reverse() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
-      */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+     Algorithms.reverse(this.grades);
    }
 
    /**
     * Sorts the array to ascending order.
     */
    public void sort() {
-      /* TODO:
-       1. Edit the test data files to see if the sort() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
-      */
-
-      int n = grades.length;
-
-      for (int i = 0; i < n - 1; i++) {
-
-         for (int j = 0; j < n - i - 1; j++) {
-
-            if (grades[j] > grades[j + 1]) {
-
-               int temp = grades[j];
-
-               grades[j] = grades[j + 1];
-               grades[j + 1] = temp;
-
-            }
-         }
-      }
+      Algorithms.sort(this.grades);
    }
 
    /**
