26b23cd3ed5245fd77852e61a03fe3a96173808d
Daanshuomingsuoyou <1906718207@qq.com>
2024-04-06 19:19:17 +0800

    SUBMIT

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,95 +1,81 @@
 package oy.tol.tra;
 
 public class Algorithms {
-
-private Algorithms(){
-
-}
-
-    public static int binarySearch(int target, Integer[] array, int from, int to) {
-
-    int Jin= from;
-    int Chu= to;
-
-        while (Jin <= Chu) {
-            int middle=(Chu+Jin)/2;
-                if (array[middle]== target) {
-                    return middle;
-                } else if (array[middle]<target) {
-                    Jin=middle+1;
-                }else {
-                    Chu=middle-1;
+    // 冒泡排序
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        boolean swapped = false;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
+            }
         }
-        return -1;
     }
 
+    // 选择排序
+    public static <T> void reverse(T [] array)  {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            T temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
-    public static int binarySearch(String value, String[] array, int from, int to) {
-
-        int Jin1= from;
-        int Chu1= to;
-
-        while (Jin1 <= Chu1) {
-            int Zhong=(Chu1+Jin1)/2;
-            int cap=value.compareTo(array[Zhong]);
-
-            if (cap== 0) {
-                return Zhong;
-            } else if (cap<0) {
-                Chu1=Zhong+1;
-            }else {
-                Jin1=Zhong-1;
+    // 插入排序
+    public static <T extends Comparable<T>> int binarySearch(T aValue, T [] fromArray, int fromIndex, int toIndex)  {
+        while (fromIndex <= toIndex) {
+            int mid = fromIndex + (toIndex - fromIndex) / 2;
+            int cmp = fromArray[mid].compareTo(aValue);
+            if (cmp == 0) {
+                return mid;
+            } else if (cmp < 0) {
+                fromIndex = mid + 1;
+            } else {
+                toIndex = mid - 1;
             }
         }
         return -1;
     }
 
+    // 快速排序
+    public static <E extends Comparable<E>> void fastSort(E [] array, int start, int end) {
+        quickSort(array, start, end);
+    }
 
-    public static  void sort(Integer[] array) {
+    private static <E extends Comparable<E>> void quickSort(E [] array, int start, int end) {
+        if (start < end) {
+            int partitionIndex = partition(array, start, end);
+            quickSort(array, start, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
 
-        int n = array.length;
+    private static <E extends Comparable<E>> int partition(E [] array, int begin, int end) {
+        E pivot = array[end];
+        int i = begin - 1;
 
-        for (int i=0;i<n-1;i++) {
-            for(int j=0;j<n-1-i;j++){
-                if (array[j]>array[j+1]){
-                    int number=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=number;
-                }
+        for (int j = begin; j < end; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                E temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
         }
-
+        E temp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = temp;
+        return i + 1;
     }
-
-    public static void quickSort(String[] array,int start,int end){
-    if (start<end){
-        int Data1= partition(array,start,end);
-        quickSort(array,start,Data1-1);
-        quickSort(array,Data1+1,end);
-    }
-    }
-
-    public static void sort(String[] array){
-    quickSort(array,0,array.length-1);
-}
-
-    public static int partition(String[] array,int start,int end){
-    String pivot =array[end];
-    int i=start-1;
-    for(int j=start; j<end; j++){
-        if (array[j].compareTo(pivot)<0){
-            i++;
-            String nonumber=array[i];
-            array[i]=array[j];
-            array[j]=nonumber;
-        }
-    }
-    String tap=array[i+1];
-    array[i+1]=array[end];
-    array[end]=tap;
-    return i+1;
-    }
-
-
 }
